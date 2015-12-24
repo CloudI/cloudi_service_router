b0fb150b3887944918203855082b0caf045d759c
Michael Truog <mjtruog@gmail.com>
2015-12-23 20:17:35 -0800

    Version 1.5.1.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CloudIServiceRouter do
 
   def project do
     [app: :cloudi_service_router,
-     version: "1.5.0",
+     version: "1.5.1",
      language: :erlang,
      description: description,
      package: package,
@@ -11,7 +11,7 @@ defmodule CloudIServiceRouter do
   end
 
   defp deps do
-    [{:cloudi_core, "~> 1.5.0"}]
+    [{:cloudi_core, "~> 1.5.1"}]
   end
 
   defp description do
@@ -20,7 +20,7 @@ defmodule CloudIServiceRouter do
 
   defp package do
     [files: ~w(src doc rebar.config README.markdown),
-     contributors: ["Michael Truog"],
+     maintainers: ["Michael Truog"],
      licenses: ["BSD"],
      links: %{"Website" => "http://cloudi.org",
               "GitHub" => "https://github.com/CloudI/" <>

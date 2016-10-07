e3b6e3d8dcf90e4fa4b583b0440ea963454efb3b
Michael Truog <mjtruog@gmail.com>
2016-10-07 13:28:01 -0700

    Version 1.5.4.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CloudIServiceRouter do
 
   def project do
     [app: :cloudi_service_router,
-     version: "1.5.3",
+     version: "1.5.4",
      language: :erlang,
      description: description,
      package: package,
@@ -11,7 +11,7 @@ defmodule CloudIServiceRouter do
   end
 
   defp deps do
-    [{:cloudi_core, "~> 1.5.3"}]
+    [{:cloudi_core, "~> 1.5.4"}]
   end
 
   defp description do
